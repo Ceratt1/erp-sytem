b83ec2a8606d33fe58361e28059dee90a0503282
cerattixo <gabrielceratticabral@gmail.com>
2025-10-26 16:10:21 -0300

    Fix InventoryController request mapping and update getAll method to accept parameters for SKU code and quantity

--- inventory/src/main/java/br/com/erpsystem/inventory/api/controllers/InventoryController.java
+++ inventory/src/main/java/br/com/erpsystem/inventory/api/controllers/InventoryController.java
@@ -3,25 +3,27 @@ package br.com.erpsystem.inventory.api.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erpsystem.inventory.usecases.InventoryUseCase;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/v1/intentories")
+@RequestMapping("/v1/inventories")
 public class InventoryController {
     
     private final InventoryUseCase useCase;
-    
+
     public InventoryController(InventoryUseCase useCase) {
         this.useCase = useCase;
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String getAll() {
-        return "OK";
+    public boolean  getAll(@RequestParam @Valid String skuCode, @RequestParam @Valid Integer quantity) {
+        return useCase.get().existsBySkuCodeAndQuantityisGreaterThanEquals(skuCode, quantity);
     }
 
 }
